f6f1b5da945e48d6c80f563cc9944c689bf0c27f
caio <caiorafaeldop@gmail.com>
2025-06-17 22:42:43 -0300

    initial commits

--- dashzap/src/app.ts
+++ dashzap/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (_req, res) => {
+app.get("/", (_req: any, res: { send: (arg0: string) => void }) => {
   res.send("✅ Dashzap API Online");
 });
 
