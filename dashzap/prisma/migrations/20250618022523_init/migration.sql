1a1a4400d4d1e3849387f8be66f8e6793bc746ba
caio <caiorafaeldop@gmail.com>
2025-06-17 23:33:07 -0300

    config bd

---
+++ dashzap/prisma/migrations/20250618022523_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Dashboard" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Dashboard_pkey" PRIMARY KEY ("id")
+);
