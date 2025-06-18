ac25a6cabe13b07837441052fa8142aa6c33f1d0
caio <caiorafaeldop@gmail.com>
2025-06-17 22:38:05 -0300

    initial commit

---
+++ dashzap/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("✅ Dashzap API Online");
+});
+
+export default app;
